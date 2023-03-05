7262372ac948e5559f9d7a29442f814d70f0ac43
Ana Santillan <anisantillan10@gmail.com>
2023-03-04 22:21:46 -0300

    Se agrego el ejercicio 5 que pide dos numeros y los suma

---
+++ js/ejercicio5.js
@@ -0,0 +1,7 @@
+// Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let numero1 = parseInt(prompt("Ingrese el primer número"));
+let numero2 = parseInt(prompt("Ingrese el segundo número"));
+let resultado = numero1 + numero2;
+
+document.write("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
